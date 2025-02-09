5e9c3f91d74b75e930c26cdad2ceb20ad837a3cb
Edalia <33891329+Edalia@users.noreply.github.com>
2025-02-09 23:24:08 +0000

    Added car array

--- header.php
+++ header.php
@@ -1,46 +1,10 @@
-		<nav class="navbar navbar-expand-lg navbar-light bg-light">
-			<a class="navbar-brand" href="#">Navbar</a>
-			<button
-				class="navbar-toggler"
-				type="button"
-				data-toggle="collapse"
-				data-target="#navbarSupportedContent"
-				aria-controls="navbarSupportedContent"
-				aria-expanded="false"
-				aria-label="Toggle navigation"
-			>
-				<span class="navbar-toggler-icon"></span>
-			</button>
+<div class="card">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<p class="navbar-nav mr-auto">Car Rental</p>
 
-			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<div class="navbar fixed-top navbar-light bg-light">
+				<a class="navbar-brand">Car Rental</a>
 				<ul class="navbar-nav mr-auto">
-					<li class="nav-item active">
-						<a class="nav-link" href="#"
-							>Home <span class="sr-only">(current)</span></a
-						>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Link</a>
-					</li>
-					<li class="nav-item dropdown">
-						<a
-							class="nav-link dropdown-toggle"
-							href="#"
-							id="navbarDropdown"
-							role="button"
-							data-toggle="dropdown"
-							aria-haspopup="true"
-							aria-expanded="false"
-						>
-							Dropdown
-						</a>
-						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-							<a class="dropdown-item" href="#">Action</a>
-							<a class="dropdown-item" href="#">Another action</a>
-							<div class="dropdown-divider"></div>
-							<a class="dropdown-item" href="#">Something else here</a>
-						</div>
-					</li>
                     <?php
 						include "dep/session.php";
                         
@@ -79,3 +43,4 @@
 				</form>
 			</div>
 		</nav>
+</div>
--- index.php
+++ index.php
@@ -20,8 +20,47 @@
 	</head>
 	<body>
 		<?php
+			$cars = array (
+						array(1,"Volvo xc60","Auto","4 seater",30,"img/volvo.jpg"),
+						array(2,"Mazda 3","Auto","4 seater",10,"img/mazda3.jpg"),
+						array(3,"Ford Ranger","Auto","4 seater",90,"img/ranger.jpg"),
+						array(4,"Nissan Juke","Auto","4 seater",25,"img/juke.jpg"),
+						array(5,"2008 VW Golf","Manual","4 seater",30,"img/golf08.jpg"),
+						array(6,"Audi A4","Auto","4 seater",40,"img/audia4.jpg")
+			);
+
 			include "header.php";
+		
 		?>
+		<div class="container text-center">
+			<div class="row">
+			<?php
+			
+			//for car in cars array, render specific cards
+			foreach($cars as $car){
+
+			?>
+				<div class="col">
+					<div class="card" style="width: 18rem;">
+					<img src=<?php echo $car[5]?> style="width: 100%; height:30vh;" alt="...">
+						<div class="card-body">
+							<h4><?php echo $car[1]?></h4>
+							<p class="card-text">Transmission: <?php echo $car[2]?></p>
+							<p class="card-text"><?php echo $car[3]?></p>
+							<p class="card-text">£ <?php echo $car[4]?> / day</p>
+							<button type="button" class="btn btn-outline-primary">Rent out <?php echo $car[1]; ?></button>
+						</div>
+					</div>
+				</div>
+
+			<?php
+
+			//end of car cards
+			}
+			?>
+			</div>
+		</div>
+		
 		<script
 			src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
 			integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
